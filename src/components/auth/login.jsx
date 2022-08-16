e733b74d4638d27a8359e24546fb20751e8c01f3
Md. Khasrur Rahman <30666985+KhasrurRahman@users.noreply.github.com>
2022-08-16 14:46:05 +0600

    Update login.jsx

--- src/components/auth/login.jsx
+++ src/components/auth/login.jsx
@@ -2,21 +2,31 @@ import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import "./login.css";
+import { useState } from "react";
 
 const Login = () => {
+  const [credential, setCredintal] = useState({});
+
+  const handelchange = (e) => {
+    setCredintal((prev) => ({
+      ...prev,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
   return (
     <div className="d-flex justify-content-center login_area">
       <Card style={{ width: "30rem" }}>
         <Card.Body>
           <h3 className="text-center text-uppercase mb-3">Login Your Account</h3>
           <Form>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Group className="mb-3">
               <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="name@example.com" />
+              <Form.Control type="email" id="email" placeholder="name@example.com" onChange={handelchange} />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="formPlaintextPassword">
+            <Form.Group className="mb-3">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control type="password" id="password" placeholder="Password" onChange={handelchange} />
             </Form.Group>
           </Form>
           <div className="text-center">
